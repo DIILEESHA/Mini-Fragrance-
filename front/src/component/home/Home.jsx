4cc08b1f44d428f84dee5bad51286f5f40263a3b
DIILEESHA <dileeeshanawarathna18@gmail.com>
2024-01-05 18:10:03 +0530

    add parallax to home

--- front/src/component/home/Home.jsx
+++ front/src/component/home/Home.jsx
@@ -6,6 +6,7 @@ import Word from "../word/Word";
 import Headershop from "../headershop/Headershop";
 import Section from "../velo/Section";
 import Mini from "../velo/Mini";
+import Parallaxd from "../paralax/Parallaxd";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Header />
       <Word />
       <WeAre />
+      <Parallaxd />
       <Headershop />
       {/* <Word /> */}
     </div>
---
+++ front/src/component/paralax/Parallaxd.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./para.css";
+import { Parallax } from "react-parallax";
+import { Link } from "react-router-dom";
+
+const Parallaxd = () => {
+  const handleClicks = () => {
+    // Scroll to the top of the SingleItemDetail component
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  return (
+    <div>
+      <Parallax
+        strength={170}
+        className="bgClassName"
+        bgImage={
+          "https://nullpunkt.co/cdn/shop/files/04_Couple_169_2.jpg?v=1669921149&width=1500"
+        }
+      >
+        <div className="content">
+          <div className="sub_content">
+            <h5>NEW COLLECTION✨</h5>
+            <h1>Be different in your own way!</h1>
+            <h2>Find your unique style.</h2>
+
+            <Link className="linka" to="/shop">
+              <button onClick={handleClicks}>shop collection🍜</button>
+            </Link>
+          </div>
+        </div>
+      </Parallax>
+    </div>
+  );
+};
+
+export default Parallaxd;
